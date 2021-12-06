dad2542a5ee612fc9f4f759837c67273b3f3b7ec
bsavitha <bsavitha.xworkz@gmail.com>
2021-12-07 02:18:58 +0530

    Collections

---
+++ src/com/xworkz/collections/ActorsCollection.java
@@ -0,0 +1,76 @@
+package com.xworkz.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ActorsCollection {
+
+	public static void main(String[] args) {
+		Collection collection = new ArrayList();
+
+		collection.add("vijay");
+		collection.add("ajith");
+		collection.add("surya");
+		collection.add("sk");
+		collection.add("vikranth");
+		collection.add("disha");
+		collection.add("karina");
+		collection.add("kapoor");
+		collection.add("sradha");
+		collection.add("lora");
+		collection.add("akshay");
+		collection.add("kumar");
+		collection.add("shreya");
+		collection.add("nayantara");
+		collection.add("Tom Hanks");
+		collection.add("Hopkins");
+		collection.add("Anthony ");
+		collection.add("Robert ");
+		collection.add("Judi ");
+		collection.add("Prabhas");
+		collection.add("Aamir Khan");
+		collection.add("vijay devarakonda");
+		collection.add("Glenn Close");
+		collection.add("nayantara");
+		collection.add("Aamir");
+		collection.add("Rajinikanth");
+		collection.add("disha patani");
+		collection.add("sidarth");
+		collection.add("sushanth");
+		collection.add("Clint Eastwood");
+		collection.add("Robert");
+		collection.add("Prabhas");
+		collection.add("Ram Charan");
+		collection.add("arya");
+		collection.add("keethi");
+		collection.add("kreti");
+		collection.add("Close");
+		collection.add("tamil");
+		collection.add("Aamir Khan");
+		collection.add("Robert");
+		collection.add("Dilip");
+		collection.add("Ravi Kishan");
+		collection.add("Radika");
+		collection.add("Yash");
+		collection.add("Prabhas");
+		collection.add("karina");
+		collection.add("Dilip Kumar");
+		collection.add("Mithun Chakraborty");
+		collection.add("Allu Arjun.");
+		collection.add("mahesh babu");
+		collection.add("Salman Khan");
+		collection.add("spanish");
+		collection.add("urdu");
+		collection.add("malyalam");
+		collection.add("Vijay Sethupathi");
+		collection.add("puneeth");
+		collection.add("Chakraborty");
+		collection.add("anuska");
+		collection.add("Kishan");
+		collection.add("anuska sharma");
+
+		System.out.println(collection.size());
+
+	}
+
+}
---
+++ src/com/xworkz/collections/SweetsCollection.java
@@ -0,0 +1,82 @@
+package com.xworkz.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SweetsCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Collection collection = new ArrayList();
+
+		collection.add("hindi");
+		collection.add("english");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("spanish");
+		collection.add("urdu");	
+		collection.add("malyalam");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("spanish");
+		collection.add("urdu");	
+		collection.add("malyalam");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("spanish");
+		collection.add("urdu");	
+		collection.add("malyalam");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("spanish");
+		collection.add("urdu");	
+		collection.add("malyalam");
+		collection.add("hindi");
+		collection.add("CHINESE");
+		collection.add("kannada");
+		collection.add("tamil");
+		collection.add("TELUGU");
+		collection.add("tamil");
+
+	
+		
+		System.out.println(collection.size());
+		
+		
+
+
+	}
+
+}
