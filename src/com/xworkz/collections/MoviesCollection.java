de9ee43ce6d62c42ecaefff96fe9342ab0d83d9f
bsavitha <91309044+bsavitha@users.noreply.github.com>
2021-12-08 02:11:06 +0530

    Update MoviesCollection.java

--- src/com/xworkz/collections/MoviesCollection.java
+++ src/com/xworkz/collections/MoviesCollection.java
@@ -1,13 +1,14 @@
 package com.xworkz.collections;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class MoviesCollection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Collection collection = new ArrayList();
+		List<String> collection = new ArrayList();
 
 		collection.add("hindi");
 		collection.add("english");
@@ -73,6 +74,22 @@ public class MoviesCollection {
 	
 		
 		System.out.println(collection.size());
+		int lastIndex = 60;
+		int firstIndex = 0;
+		int mid = firstIndex + (lastIndex - firstIndex) / 2;
+		System.out.println("Middle element is at index :");
+		System.out.println(mid);
+
+		if (mid == mid && mid < collection.size()) {
+			System.out.println("Middle element present ? : ".concat(String.valueOf(collection.contains(mid))));
+			 System.out.println("Removing.. " .concat(String.valueOf(collection.remove(mid) +" from index : " .concat(String.valueOf(mid)))));
+		}
+		
+		System.out.println("Displaying...........");
+		for (int i = 0; i < collection.size(); i++) {
+			System.out.println(i);
+
+		}
 		
 		
 	}
