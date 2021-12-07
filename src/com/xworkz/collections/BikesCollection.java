e26e74d3906330efdc5305fa5bed7c352acefc4d
bsavitha <91309044+bsavitha@users.noreply.github.com>
2021-12-07 07:39:14 +0530

    Update BikesCollection.java

--- src/com/xworkz/collections/BikesCollection.java
+++ src/com/xworkz/collections/BikesCollection.java
@@ -9,19 +9,19 @@ public class BikesCollection {
 		// TODO Auto-generated method stub
 		Collection collection = new ArrayList();
 
-		collection.add("hindi");
-		collection.add("english");
-		collection.add("kannada");
-		collection.add("tamil");
-		collection.add("TELUGU");
-		collection.add("spanish");
-		collection.add("urdu");	
-		collection.add("malyalam");
-		collection.add("hindi");
-		collection.add("CHINESE");
-		collection.add("kannada");
-		collection.add("tamil");
-		collection.add("TELUGU");
+		collection.add("Bajaj");
+		collection.add("pulsur");
+		collection.add("honda");
+		collection.add("hero");
+		collection.add("hero passion");
+		collection.add("apache");
+		collection.add("tvs");	
+		collection.add("ntorq");
+		collection.add("yamaha");
+		collection.add("KTM");
+		collection.add("duke");
+		collection.add("splender");
+		collection.add("R15");
 		collection.add("spanish");
 		collection.add("urdu");	
 		collection.add("malyalam");
