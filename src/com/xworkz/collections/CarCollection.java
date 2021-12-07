b36546a5f9d4948c960b2478a70fd3e1ffc45b06
bsavitha <91309044+bsavitha@users.noreply.github.com>
2021-12-08 02:04:51 +0530

    Update CarCollection.java

--- src/com/xworkz/collections/CarCollection.java
+++ src/com/xworkz/collections/CarCollection.java
@@ -1,13 +1,15 @@
 package com.xworkz.collections;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class CarCollection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Collection collection = new ArrayList();
+	List<String> collection = new ArrayList();
 
 		collection.add("BMW");
 		collection.add("audi");
@@ -73,7 +75,22 @@ public class CarCollection {
 	
 		
 		System.out.println(collection.size());
+		int lastIndex = 60;
+		int firstIndex = 0;
+		int mid = firstIndex + (lastIndex - firstIndex) / 2;
+		System.out.println("Middle element is at index :");
+		System.out.println(mid);
+
+		if (mid == mid && mid < collection.size()) {
+			System.out.println("Middle element present ? : ".concat(String.valueOf(collection.contains(mid))));
+			 System.out.println("Removing.. " .concat(String.valueOf(collection.remove(mid) +" from index : " .concat(String.valueOf(mid)))));
+		}
 		
+		System.out.println("Displaying...........");
+		for (int i = 0; i < collection.size(); i++) {
+			System.out.println(i);
+
+		}
 		
 
 	}
